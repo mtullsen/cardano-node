31c5fa06eec3a74b2ec8ffbfefeaa2e001ee0dab
John Ky <john.ky@iohk.io>
2023-04-11 16:44:39 +1000

    New ProtocolUpdateUTxOCostPerWordSupportedInEra type

--- cardano-api/src/Cardano/Api.hs
+++ cardano-api/src/Cardano/Api.hs
@@ -284,9 +284,11 @@ module Cardano.Api (
 
     -- ** Era-dependent protocol features
     ProtocolUTxOCostPerByteSupportedInEra(..),
+    ProtocolUTxOCostPerWordSupportedInEra(..),
 
     -- ** Era-dependent protocol feature availability functions
     protocolUTxOCostPerByteSupportedInEra,
+    protocolUTxOCostPerWordSupportedInEra,
 
     -- ** Fee calculation
     LedgerEpochInfo(..),
